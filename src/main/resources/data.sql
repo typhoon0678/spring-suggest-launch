14cad5d53591c61a543f8edb3475c949c0369d22
typhoon0678 <typhoon0678@gmail.com>
2024-05-08 09:15:15 +0900

    design: html 작성 kakao map, header

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당1', 37.123456, 126.987654, NOW(), NOW());
-INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당2', 37.123456, 126.987654, NOW(), NOW());
-INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당3', 37.123456, 126.987654, NOW(), NOW());
+INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당1', 37.572467, 126.990367, NOW(), NOW());
+INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당2', 37.573467, 126.992367, NOW(), NOW());
+INSERT INTO restaurant (name, lat, lon, created_at, updated_at) VALUES ('식당3', 37.574467, 126.994367, NOW(), NOW());
